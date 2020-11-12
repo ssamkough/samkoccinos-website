80408d7ed5a4b22387a885c1e0e7a7147188a1d0
Sammy Samkough <sammysamkough@gmail.com>
2020-11-11 20:27:36 -0500

    fixed styles of podcast 1 item page, replaced image

--- components/podcastItem.php
+++ components/podcastItem.php
@@ -7,10 +7,10 @@
         <p class=\"podcast-item-description\">$podcastDescription</p>
         <hr />
         <ul class=\"podcast-item-links\">
-            <li><a class=\"podcast-spotify-link\" href=\"$podcastSpotify\">Spotify</a></li>
-            <li><a class=\"podcast-google-link\" href=\"$podcastGoogle\">Google</a></li>
-            <li><a class=\"podcast-apple-link\" href=\"$podcastApple\">Apple</a></li>
             <li><a class=\"podcast-yt-link\" href=\"$podcastYoutube\">YouTube</a></li> 
+            <li><a class=\"podcast-spotify-link\" href=\"$podcastSpotify\">Spotify</a></li>
+            <li><a class=\"podcast-apple-link\" href=\"$podcastApple\">Apple</a></li>
+            <li><a class=\"podcast-google-link\" href=\"$podcastGoogle\">Google</a></li>
             <li><a class=\"podcast-link\" href=\"$podcastMoreInfo\">More Info</a></li> 
         </ul>
     </div> ";
--- podcastItems/1.php
+++ podcastItems/1.php
@@ -7,13 +7,13 @@
           <div class="podcast-item-page">
             <h2 class="podcast-title-page">JebTube - Samkoccinos #1</h2>
             <h3 class="podcast-published-date-page">Sunday, November 8</h3>
-            <p class="podcast-item-description-page"></p>Geeking out with JebTube, an analysis YouTuber covering video games (mostly Sega as to date).
+            <p class="podcast-item-description-page">Geeking out with JebTube, an analysis YouTuber covering video games (mostly Sega as to date).</p>
             <img class="podcast-image-page" alt="Samkcoccinos/JebTube Art Cover" src="./assets/artwork/podcastItemArt001.png" />
             <ul class="podcast-item-links">
-                <li><a class="podcast-spotify-link" href="$podcastSpotify">Spotify</a></li>
-                <li><a class="podcast-google-link" href="$podcastGoogle">Google</a></li>
-                <li><a class="podcast-apple-link" href="$podcastApple">Apple</a></li>
-                <li><a class="podcast-yt-link" href="$podcastYoutube">YouTube</a></li> 
+                <li><a class="podcast-yt-link" href="">YouTube</a></li> 
+                <li><a class="podcast-spotify-link" href="">Spotify</a></li>
+                <li><a class="podcast-apple-link" href="">Apple</a></li>
+                <li><a class="podcast-google-link" href="">Google</a></li>
             </ul>
         </div>
         </div>
